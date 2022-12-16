1670fbd155481f9b69ae257dd65ac46716c9712a
Carlos Calvo <52244870+ccalvocm@users.noreply.github.com>
2022-12-16 10:23:09 -0300

    add: postProcess

--- simcopiapo/postProcess.py
+++ simcopiapo/postProcess.py
@@ -19,9 +19,13 @@ def main():
     output_dir=os.path.join('.','modelOutputs')
     well_output_dir = os.path.join(output_dir, "WEL")
     dis_output_dir = os.path.join(output_dir, "DIS")
-    
+    bas_output_dir = os.path.join(output_dir, "BAS")
+    upw_output_dir = os.path.join(output_dir, "UPW")
+    lpf_output_dir = os.path.join(output_dir, "LPF")
+
     # create the vtk object and export heads
     nam_file=os.path.join(model_ws, "SIMCOPIAPO.nam")
+    nam_file=r'G:\OneDrive - ciren.cl\2022_Ficha_Atacama\05_Modelos\DGA-DICTUC\MODFLOW Transiente\Copiapo_12102022.nam'
     # load model
     ml = flopy.modflow.Modflow.load(nam_file, model_ws='.', check=False)
 
@@ -38,3 +42,11 @@ def exportWel(ml,well_output_dir):
 def exportDis(ml,dis_output_dir):
     ml.dis.export(dis_output_dir, fmt="vtk")
 
+def exportBas(ml,bas_output_dir):
+    ml.bas6.export(bas_output_dir, fmt="vtk")
+
+def exportUpw(ml,upw_output_dir):
+    ml.upw.export(upw_output_dir, fmt="vtk")
+
+def exportLpf(ml,lpf_output_dir):
+    ml.lpf.export(lpf_output_dir, fmt="vtk")
